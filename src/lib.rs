81d3cded0af4fe5f631310d10aca19c5f900c2c0
indianeros58 <0xDEADBEAT@gmail.com>
2022-08-17 04:50:09 -0400

    ?

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
 pub const PHYSICAL_ADDRESS: &[u8] = &[];
 pub const DIGITAL_ADDRESS: &[u8] = &[];
 pub const DISPUTE_ADDRESS: &[u8] = &[];
+
+pub const PHYSICAL_SIGNER: &[u8] = &[];
+pub const DIGITAL_SIGNER: &[u8] = &[];
+pub const DISPUTE_SIGNER: &[u8] = &[];
